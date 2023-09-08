3079e4bf7338a1d0fad41d6455202730da6dff59
Gayathri Mantha <gayathrimantha16@gmail.com>
2023-09-08 13:02:57 +0530

    commit

---
+++ src/assets/index.js
@@ -0,0 +1,81 @@
+import logo from "./logo.svg";
+import gayathrilogo from "./gayathrilogo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import mysql from "./tech/mysql.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import ehred from "./company/ehred.png";
+import ehwhite from "./company/ehwhite.png";
+import accenture from "./company/accenture.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import engineershub from "./engineershub.png";
+import aliensfest from "./aliensfest.png";
+import todo from "./todo.png";
+import netflix from "./netflix.jpeg";
+
+
+export {
+  logo,
+  gayathrilogo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  mysql,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  ehred,
+  ehwhite,
+  accenture,
+  engineershub,
+  aliensfest,
+  todo,
+  netflix,
+};
